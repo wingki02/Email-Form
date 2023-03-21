137ef09fa560d0db665e105dc7be09ddac2f08b1
wingki sandro marganda simbolon <93369185+wingki02@users.noreply.github.com>
2023-03-21 10:29:27 +0700

    Initial commit

---
+++ app/src/main/java/com/wingki_202102318/emailform/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wingki_202102318.emailform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
